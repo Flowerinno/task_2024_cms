fae812eb5a6d79908145b6175d606a54109f70d1
Aleksandr Kononov <oleksandr.kononov@devit.group>
2024-03-28 23:41:38 +0200

    refactor: change anchors in dashboard-nav to next/links

--- components/ui/nav/dashboard-nav.tsx
+++ components/ui/nav/dashboard-nav.tsx
@@ -12,6 +12,7 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from '@/components/ui/navigation-menu'
+import Link from 'next/link'
 
 const componentsFeed: { title: string; href: string; description: string }[] = [
   {
@@ -58,6 +59,23 @@ const componentsAdvertisement: {
   },
 ]
 
+const componentsUsers: {
+  title: string
+  href: string
+  description: string
+}[] = [
+  {
+    title: 'List',
+    href: '/dashboard/users',
+    description: 'List of users.',
+  },
+  {
+    title: 'Create user',
+    href: '/dashboard/users/create',
+    description: 'Create a new user.',
+  },
+]
+
 export function DashboardNavigationMenu() {
   return (
     <NavigationMenu className='p-2'>
@@ -65,13 +83,12 @@ export function DashboardNavigationMenu() {
         <NavigationMenuItem>
           <NavigationMenuTrigger>Users</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className='grid gap-3 grid-cols-3 p-6 md:w-[400px] lg:w-[500px]'>
-              <ListItem href='/dashboard/users' title='List'>
-                List of users
-              </ListItem>
-              <ListItem href='/dashboard/users/create' title='Create'>
-                Create user
-              </ListItem>
+            <ul className='grid gap-3 grid-cols-2 p-6 md:w-[500px] lg:w-[600px]'>
+              {componentsUsers.map((component) => (
+                <ListItem key={component.title} href={component.href} title={component.title}>
+                  {component.description}
+                </ListItem>
+              ))}
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
@@ -105,12 +122,16 @@ export function DashboardNavigationMenu() {
 }
 
 const ListItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(
-  ({ className, title, children, ...props }, ref) => {
+  ({ className, title, href, children, ...props }, ref) => {
+    if (!href) return null
+
     return (
       <li>
         <NavigationMenuLink asChild>
-          <a
+          <Link
             ref={ref}
+            prefetch={true}
+            href={href}
             className={cn(
               'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
               className,
@@ -119,7 +140,7 @@ const ListItem = React.forwardRef<React.ElementRef<'a'>, React.ComponentPropsWit
           >
             <div className='text-sm font-medium leading-none'>{title}</div>
             <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>{children}</p>
-          </a>
+          </Link>
         </NavigationMenuLink>
       </li>
     )
